6b831da5a96fe5486510c99693abf0e89c2c2100
Ramon Rico Azagra <ramonrico92@gmail.com>
2021-02-28 13:07:33 +0100

    comentario

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,6 +20,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "string.h"
+//HOLA
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
